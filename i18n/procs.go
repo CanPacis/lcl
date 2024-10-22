fa83b1a370891f8617380295dacb0f2c4cf0a7cd
Muhammed Ali CAN <tmwwdw@gmail.com>
2024-10-22 18:19:43 +0300

    chore: remove files

--- i18n/procs.go
+++
@@ -1,7 +0,0 @@
-package i18n
-
-import "time"
-
-func proc_year(t time.Time) string {
-	return strconv_ext_i.Itoa(time_ext_i.Year(t))
-}
--- i18n/types.go
+++
@@ -1,6 +0,0 @@
-package i18n
-
-type User struct {
-	Name string
-	Age  int
-}
